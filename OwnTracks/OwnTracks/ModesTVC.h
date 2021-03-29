c3606ad1e3daef177f39b7fbcc7448498bdc949e
Christoph Krey <c@ckrey.de>
2021-03-29 20:53:47 +0200

    [FIX] translate modes screen (textviews)

---
+++ OwnTracks/OwnTracks/ModesTVC.h
@@ -0,0 +1,17 @@
+//
+//  ModesTVC.h
+//  OwnTracks
+//
+//  Created by Christoph Krey on 29.03.21.
+//  Copyright © 2021 OwnTracks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ModesTVC : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ OwnTracks/OwnTracks/ModesTVC.m
@@ -0,0 +1,28 @@
+//
+//  ModesTVC.m
+//  OwnTracks
+//
+//  Created by Christoph Krey on 29.03.21.
+//  Copyright © 2021 OwnTracks. All rights reserved.
+//
+
+#import "ModesTVC.h"
+
+@interface ModesTVC ()
+@property (weak, nonatomic) IBOutlet UITextView *mqttDescription;
+@property (weak, nonatomic) IBOutlet UITextView *httpDescription;
+@property (weak, nonatomic) IBOutlet UITextView *pleaseNote;
+
+@end
+
+@implementation ModesTVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.mqttDescription.text = NSLocalizedString(@"Setup your own OwnTracks server for full privacy protection. More Info on https://owntracks.org/booklet", @"MQTT Description Text");
+    self.httpDescription.text = NSLocalizedString(@"Similar to MQTT mode, except data transmission uses HTTP, not MQTT.", @"HTTP Description Text");
+    self.pleaseNote.text = NSLocalizedString(@"When switching between modes, all OwnTracks data will be deleted for privacy reasons.", @"Please Note Text");
+}
+
+
+@end
