8041b6ef83f9149969dfdc7fbedcf18148c5b63c
Christoph Krey <c@ckrey.de>
2024-01-06 09:08:23 +0100

    [FIX] remove Watch from Pods

--- OwnTracks/Pods/Target Support Files/Pods-OwnTracksWrist WatchKit App/Pods-OwnTracksWrist WatchKit App-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_OwnTracksWrist_WatchKit_App : NSObject
-@end
-@implementation PodsDummy_Pods_OwnTracksWrist_WatchKit_App
-@end
--- OwnTracks/Pods/Target Support Files/mqttc-iOS/mqttc-iOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_mqttc_iOS : NSObject
-@end
-@implementation PodsDummy_mqttc_iOS
-@end
